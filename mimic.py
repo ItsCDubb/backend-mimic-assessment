7005d9b3535715d186a59eca340515d7d4d7986a
Chris Warren <chris@chriswarren.dev>
2020-05-24 11:30:15 -0400

    Initial Commit

--- mimic.py
+++ mimic.py
@@ -5,7 +5,8 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-"""Mimic exercise
+"""
+Mimic exercise
 
 Read in the file specified on the command line.
 Do a simple split() on whitespace to obtain all the words in the file.
@@ -19,11 +20,10 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "Chris Warren with help from Cheria Artis & Stack Overflow"
+__author__ = "Chris Warren with help from Cheria Artis, Piero Madar, & Stack Overflow"
 
 import random
 import sys
-
 
 def create_mimic_dict(filename):
     """
@@ -45,9 +45,8 @@ def create_mimic_dict(filename):
             }
     """
     mimic_dict = {}
-    file = open(filename, 'r')
-    text = file.read()
-    file.close()
+    with open(filename, 'r') as file:
+        text = file.read()
     words = text.split()
     prev_word = ''
     for word in words:
@@ -60,22 +59,23 @@ def create_mimic_dict(filename):
 
     return mimic_dict
 
-def print_mimic(mimic_dict, word):
+def print_mimic(mimic_dict, start_word):
     """
-    Given a previously created mimic_dict and start word,
+    Given a previously created mimic_dict and start_word,
     prints 200 random words from mimic_dict as follows:
         - Print the start word
         - Look up the start word in your mimic_dict and get its next-list
         - Randomly select a new word from the next-list
         - Repeat this process 200 times
     """
-    values = [word]
+    start_word = random.choice(list(mimic_dict.keys()))
     for i in range(200):
-        word = random.choice(mimic_dict.get(word))
-        values.append(word)
-    print(' '.join(values))
-    return ' '.join(values)
-
+        print(start_word, end= " ")
+        next_word_list = mimic_dict.get(start_word)
+        if next_word_list is None:
+            next_word_list = mimic_dict[""]
+        start_word = random.choice(next_word_list)
+        
 def main():
     if len(sys.argv) != 2:
         print('usage: python mimic.py file-to-read')
@@ -83,7 +83,6 @@ def main():
 
     d = create_mimic_dict(sys.argv[1])
     print_mimic(d, '')
-
 
 if __name__ == '__main__':
     main()
